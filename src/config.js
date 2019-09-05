145001d0fc3e5c38fc2502964dabd127fde91ae5
satyalimon <disatyameas@gmail.com>
2019-09-05 20:57:37 +0700

    x2

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  apiKey: "AIzaSyBLFhA_jxWhmThZ22AX4RKmOR1INgU4enM",
+  authDomain: "awesome-7c899.firebaseapp.com",
+  databaseURL: "https://awesome-7c899.firebaseio.com"
+};
+
+export default config;
