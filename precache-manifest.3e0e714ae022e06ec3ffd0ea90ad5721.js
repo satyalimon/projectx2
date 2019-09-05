5d9626816c8fd59de2ff8ff7648591d8f6e044ae
satyalimon <disatyameas@gmail.com>
2019-09-05 21:00:39 +0700

    Updates

---
+++ precache-manifest.3e0e714ae022e06ec3ffd0ea90ad5721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50add53dfa98b3fe2d005464e29e07bd",
+    "url": "/projectx2/index.html"
+  },
+  {
+    "revision": "e9e0fe0ef50fa019cdc3",
+    "url": "/projectx2/static/css/2.81ef2a82.chunk.css"
+  },
+  {
+    "revision": "e9e0fe0ef50fa019cdc3",
+    "url": "/projectx2/static/js/2.5ac8410c.chunk.js"
+  },
+  {
+    "revision": "5138eb907fc710334dd5",
+    "url": "/projectx2/static/js/main.ccc906f2.chunk.js"
+  },
+  {
+    "revision": "1051427a4ead63c5d687",
+    "url": "/projectx2/static/js/runtime~main.58a05185.js"
+  }
+]);
